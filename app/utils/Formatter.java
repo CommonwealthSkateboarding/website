f21785bfa897dcfac1d5cc5b9d40635b2faffaec
Colin Delargy <colin.delargy@gmail.com>
2014-12-30 21:37:52 -0500

    Add gravatar url builder for later use

--- app/utils/Formatter.java
+++ app/utils/Formatter.java
@@ -1,5 +1,10 @@
 package utils;
 
+import models.skatepark.Membership;
+import play.Logger;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.text.DecimalFormat;
 
 /**
@@ -13,5 +18,33 @@ public class Formatter {
         } else {
             return ("$0.00");
         }
+    }
+
+    private static String hex(byte[] array) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < array.length; ++i) {
+            sb.append(Integer.toHexString((array[i]
+                    & 0xFF) | 0x100).substring(1,3));
+        }
+        return sb.toString();
+    }
+    private static String md5Hex (String message) {
+        try {
+            MessageDigest md =
+                    MessageDigest.getInstance("MD5");
+            return hex(md.digest(message.getBytes("CP1252")));
+        } catch (NoSuchAlgorithmException e) {
+            Logger.error("Cannot create md5 hash", e);
+        } catch (UnsupportedEncodingException e) {
+            Logger.error("Cannot create md5 hash", e);
+        }
+        return null;
+    }
+
+    public static String getGravatarUrl(Membership membership) {
+        if (null != membership && null != membership.email) {
+            return "http://www.gravatar.com/avatar/" + md5Hex(membership.email);
+        }
+        return null;
     }
 }
