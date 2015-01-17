97ee585a52970a85d70402cd6fc3da3a69eea90b
Colin Delargy <colin.delargy@gmail.com>
2015-01-17 00:23:28 -0500

    Use toString instead of asText

--- app/controllers/Square.java
+++ app/controllers/Square.java
@@ -4,10 +4,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import models.square.Order;
 import models.square.Payment;
-import models.square.PaymentItemization;
 import models.square.SquareWebhook;
 import play.Logger;
-import play.api.libs.json.Json;
 import play.libs.ws.*;
 import play.libs.F.Promise;
 import play.mvc.Controller;
@@ -15,7 +13,6 @@ import play.mvc.Result;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -62,7 +59,6 @@ public class Square extends Controller {
     private static Payment getPayment(String paymentId) {
         WSRequestHolder holder = WS.url(PAYMENT_URL + paymentId).setHeader(AUTHORIZATION_HEADER, BEARER_TOKEN);
         WSResponse response = holder.get().get(10000);
-        //System.out.println(response.getBody());
         Payment payment = null;
         try {
             ObjectMapper mapper = new ObjectMapper();
@@ -75,10 +71,9 @@ public class Square extends Controller {
 
     public static Result receiveWebhook() {
         SquareWebhook hook = null;
-        Logger.info("Receiving webhook: " + request().body().asJson().asText());
         try {
             ObjectMapper mapper = new ObjectMapper();
-            hook = mapper.readValue(request().body().asJson().asText(), SquareWebhook.class);
+            hook = mapper.readValue(request().body().asJson().toString(), SquareWebhook.class);
         } catch (IOException e) {
             Logger.error("Bad conversion of square webhook invocation", e);
         }
