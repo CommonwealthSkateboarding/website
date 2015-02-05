e7510484a51035349a1cc06e7cba0bf7c69635a1
Mandy McClausky <mandy.mcclausky@gmail.com>
2015-02-04 18:08:16 -0800

    Responsive stuff.

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,59 @@
 $(document).ready(function(){
 
-	$('#carousel').slick({
+	var caro = $("#carousel");
+	var header = $("header#main");
+	
+	caro.slick({
 		autoplay: true,
-		autoplaySpeed: 5000,
+		autoplaySpeed: 3000,
 		arrows: false,
 		slidesToScroll: 1,
 		dots: true,
 		infinite: true,
-		speed: 500,
+		speed: 1500,
 		slide: '.slide',
-		cssEase: 'linear',
-		pauseOnHover: false,
-		fade: true
+		cssEase: 'ease-out',
+		pauseOnHover: true,
+		fade: true,
+
+	// 	responsive: [
+	// 	{
+	// 		breakpoint: 480,
+	// 		settings: {
+	// 			autoplay: false
+	// 		}
+	// 	}
+	// ]
 	});
 
+	// Custom Carousel Arrows
 	$('.prev.arrow').click(function() {
-		$('#carousel').slickPrev();
+		caro.slickPrev();
 	});
 
 	$('.next.arrow').click(function() {
-		$('#carousel').slickNext();
+		caro.slickNext();
 	});
+
+	// Toggle Removal of Carousel @ 600px
+	var mq6 = matchMedia("screen and (max-width: 600px)");
+	mq6.addListener(function(mq6) {
+			if (mq6.matches) {
+				caro.detach();
+			} else {
+				caro.insertAfter(header);
+			}
+	});
+
+	// Remove Carousel Altogether on Mobile
+	var mqm = matchMedia("screen and (max-device-width: 420px)"),
+		handleMQL = function(mqm) {
+		if (mqm.matches) {
+			caro.detach();
+		}
+	};
+	handleMQL(mqm);
+	mqm.addListener(handleMQL);
+		
+
 });
