2afa03f71d23aa1cfe1acfb7ad266168c07bb1dc
Mandy McClausky <mandy.mcclausky@gmail.com>
2018-04-01 13:15:46 -0700

    nm

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,3 @@
-// Refresh on Orientation Change
-if (window.DeviceOrientationEvent) {
-  window.addEventListener(
-    "orientationchange",
-    function() {
-      location.reload();
-    },
-    false
-  );
-}
-
 $(document).ready(function() {
   var caro = $("#carousel");
   var header = $("header#main");
